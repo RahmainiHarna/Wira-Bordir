ddd165cc215fcc6982042783117ddf4170b524a4
Nazwa Sahara <nazwasahraa15@gmail.com>
2025-12-13 15:45:16 +0700

    customer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,40 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'username' => 'admin',
+            'email' => 'admin@example.com',
+            'phone_number' => '081234567890',
+            'address' => 'Jakarta, Indonesia',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'is_active' => true,
         ]);
+
+        // Create Owner User
+        User::create([
+            'username' => 'owner',
+            'email' => 'owner@example.com',
+            'phone_number' => '081234567891',
+            'address' => 'Bandung, Indonesia',
+            'password' => Hash::make('password'),
+            'role' => 'owner',
+            'is_active' => true,
+        ]);
+
+        // Create Customer User
+        User::create([
+            'username' => 'customer',
+            'email' => 'customer@example.com',
+            'phone_number' => '081234567892',
+            'address' => 'Surabaya, Indonesia',
+            'password' => Hash::make('password'),
+            'role' => 'customer',
+            'is_active' => true,
+        ]);
+
+        // $this->call([
+        //     OrderSeeder::class,
+        // ]);
     }
 }
